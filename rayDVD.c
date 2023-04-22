5a230c9dd1133133ec54f8cdcc01e82e2fe6230a
pseudoclone <prince.077bei27@acem.edu.np>
2023-04-22 10:01:29 +0545

    add: DVD bounce animation using Raylib

---
+++ rayDVD.c
@@ -0,0 +1,65 @@
+#include <raylib.h>
+int main(int argc, char *argv[])
+{
+	const int screenHeight = 450;
+	const int screenWidth = 800;
+	InitWindow(screenWidth, screenHeight, "DVD Logo");
+	SetTargetFPS(60);
+
+
+
+	Image image = LoadImage("/home/diethyl/dvd.png");
+	Texture2D texture = LoadTextureFromImage(image);
+	UnloadImage(image);
+	image = LoadImageFromTexture(texture);
+
+
+	UnloadTexture(texture);
+	texture = LoadTextureFromImage(image);
+	UnloadImage(image);
+
+	SetExitKey(KEY_ESCAPE);
+
+	/*Camera camera = {
+	  .position = {0.0f, 0.0f, 0.0f},
+	  .target   = {0.0f, 0.0f, 1.0f},
+	  .up       = {0.0f, 1.0f, 0.0f},
+	  .fovy     = 70.0f
+	  };*/
+
+	int initWidth = screenWidth/2;
+	int initHeight = screenHeight/2;
+
+
+	while(!WindowShouldClose())
+	{
+		ClearBackground(BLACK);
+		while(1) {
+			if(initWidth >= screenWidth || initHeight >= screenHeight) {
+				while(initWidth!=0 || initHeight != 0) {
+
+
+					initHeight--; 
+					initWidth--;
+
+					BeginDrawing();
+					DrawTexture(texture, initWidth, initHeight, WHITE);
+					ClearBackground(BLACK);
+					EndDrawing();
+				}
+			}
+			else {
+				initHeight++;
+				initWidth++;
+				BeginDrawing();
+				DrawTexture(texture, initWidth, initHeight, WHITE);
+				ClearBackground(BLACK);
+				EndDrawing();
+			}
+
+
+		}
+		CloseWindow();
+	}
+	return 0;
+}
