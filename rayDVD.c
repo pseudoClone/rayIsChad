057effe8e914f23142fb08af4075138ef217f974
pseudoclone <prince.077bei27@acem.edu.np>
2023-04-22 16:37:12 +0545

    remove: ray binary update: collision using edges checking function -> renderxp

--- rayDVD.c
+++ rayDVD.c
@@ -1,8 +1,22 @@
 #include <raylib.h>
+
+const int screenHeight = 450;
+const int screenWidth = 800;
+
+
+static void inline renderxp(Texture2D texture, int initHeight, int initWidth)
+{
+
+	BeginDrawing();
+	DrawTexture(texture, initWidth, initHeight, WHITE);
+	ClearBackground(BLACK);
+	EndDrawing();
+}
+
 int main(int argc, char *argv[])
 {
-	const int screenHeight = 450;
-	const int screenWidth = 800;
+	
+	
 	InitWindow(screenWidth, screenHeight, "DVD Logo");
 	SetTargetFPS(60);
 
@@ -26,26 +40,28 @@ int main(int argc, char *argv[])
 	while(!WindowShouldClose()){
 		ClearBackground(BLACK);
 		while(1) {
-			if(initWidth+texture.width >= screenWidth || initHeight+texture.height >= screenHeight) {
-				while(initWidth+texture.width!=0 || initHeight+texture.height != 0) {
+			initHeight--;
+			initWidth--;
+			BeginDrawing();
+			DrawTexture(texture, initWidth, initHeight, WHITE);
+			ClearBackground(BLACK);
+			EndDrawing();
+			if(initWidth - (texture.width/2) >= screenWidth) {
+				initWidth = screenWidth - texture.width;
+				renderxp(texture, initHeight, initWidth);
 
-
-					initHeight--; 
-					initWidth--;
-
-					BeginDrawing();
-					DrawTexture(texture, initWidth, initHeight, WHITE);
-					ClearBackground(BLACK);
-					EndDrawing();
-				}
 			}
-			else {
-				initHeight++;
-				initWidth++;
-				BeginDrawing();
-				DrawTexture(texture, initWidth, initHeight, WHITE);
-				ClearBackground(BLACK);
-				EndDrawing();
+			else if((initWidth - texture.width) < 0) {
+				initWidth = initWidth;
+				renderxp(texture, initHeight, initWidth);
+			}
+			if((initHeight - texture.height) < 0) {
+				initHeight = initHeight;
+				renderxp(texture, initHeight, initWidth);
+			}
+			else if((initHeight - texture.height/2) >= screenHeight) {
+				initHeight = screenHeight - texture.height;
+				renderxp(texture, initHeight, initWidth);
 			}
 
 
