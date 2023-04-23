35a1721352eae001146d6e70d4a53bc9f30e7f05
pseudoclone <prince.077bei27@acem.edu.np>
2023-04-23 07:48:32 +0545

    revision: add usless variables to make more complex

--- rayDVD.c
+++ rayDVD.c
@@ -3,70 +3,64 @@
 const int screenHeight = 450;
 const int screenWidth = 800;
 
+int img_vel = 1;
 
-static void inline renderxp(Texture2D texture, int initHeight, int initWidth)
+int img_dx = 1;
+int img_dy = -1;
+
+int initWidth = screenWidth / 2;
+int initHeight = screenHeight / 2;
+
+
+void bounce(Image image, Texture2D texture)
 {
+	int area = texture.height * texture.width;
+	initWidth += (img_dx * img_vel);
+	initHeight += (img_dy * img_vel);
 
+	if(initWidth + texture.width > screenWidth) {
+		initWidth = screenWidth - area / 2 * 1.2;
+		img_dx = -1;
+	}
+	else if(initWidth < 0) {
+		initWidth = initWidth;
+		img_dx = 1;
+	}
+
+	if(initHeight < 0) {
+		initHeight = initHeight;
+		img_dy = 1;
+	}
+	else if(initHeight + texture.height > screenHeight) {
+		initHeight = screenHeight - area / 2 * 1.2;
+		img_dy = -1;
+	}
+
+}
+
+void renderxp(Image image, Texture2D texture)
+{
 	BeginDrawing();
-	DrawTexture(texture, initWidth, initHeight, WHITE);
 	ClearBackground(BLACK);
+	DrawTexture(texture, initWidth, initWidth, WHITE);
 	EndDrawing();
 }
 
 int main(int argc, char *argv[])
 {
-	
-	
-	InitWindow(screenWidth, screenHeight, "DVD Logo");
-	SetTargetFPS(60);
+	InitWindow(screenWidth, screenHeight, "DVD");
 
+	SetTargetFPS(60);
 
 	Image image = LoadImage("dvd.png");
 	Texture2D texture = LoadTextureFromImage(image);
 	UnloadImage(image);
-	image = LoadImageFromTexture(texture);
-
-
-	UnloadTexture(texture);
-	texture = LoadTextureFromImage(image);
-	UnloadImage(image);
 
 	SetExitKey(KEY_ESCAPE);
-
-	int initWidth = screenWidth/2;
-	int initHeight = screenHeight/2;
-
-
-	while(!WindowShouldClose()){
-		ClearBackground(BLACK);
-		while(1) {
-			initHeight--;
-			initWidth--;
-			BeginDrawing();
-			DrawTexture(texture, initWidth, initHeight, WHITE);
-			ClearBackground(BLACK);
-			EndDrawing();
-			if(initWidth - (texture.width/2) >= screenWidth) {
-				initWidth = screenWidth - texture.width;
-				renderxp(texture, initHeight, initWidth);
-
-			}
-			else if((initWidth - texture.width) < 0) {
-				initWidth = initWidth;
-				renderxp(texture, initHeight, initWidth);
-			}
-			if((initHeight - texture.height) < 0) {
-				initHeight = initHeight;
-				renderxp(texture, initHeight, initWidth);
-			}
-			else if((initHeight - texture.height/2) >= screenHeight) {
-				initHeight = screenHeight - texture.height;
-				renderxp(texture, initHeight, initWidth);
-			}
-
-
-		}
-		CloseWindow();
+	while (!WindowShouldClose()) {
+		bounce(image, texture);
+		renderxp(image, texture);
 	}
+	CloseWindow();
 	return 0;
 }
