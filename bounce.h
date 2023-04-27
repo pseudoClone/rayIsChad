565f7176a1ab16954fe1adbb6355d1859bb0c681
pseudoclone <prince.077bei27@acem.edu.np>
2023-04-27 21:25:58 +0545

    Revert "revert: final commit"
    This reverts commit 39f57f55ce73417fc4b760308900bc8979cc373b.

--- bounce.h
+++
@@ -1,8 +0,0 @@
-#include <raylib.h>
-
-#ifndef __BOUNCE_H_
-#define __BOUNCE_H_
-#include <raylib.h>
-
-void bounce(Image image, Texture2D texture);
-#endif
--- rayDVD.c
+++ rayDVD.c
@@ -1,27 +1,53 @@
 #include <raylib.h>
-#include "render.h"
-#include "bounce.h"
-
 
 const int screenHeight = 450;
 const int screenWidth = 800;
 
-extern int img_vel;
+int img_vel = 1;
 
-extern int img_dx;
-extern int img_dy;
+int img_dx = 1;
+int img_dy = -1;
 
-extern int initWidth;
-extern int initHeight;
+int initWidth = screenWidth / 2;
+int initHeight = screenHeight / 2;
 
 
+void bounce(Image image, Texture2D texture)
+{
+	int area = texture.height * texture.width;
+	initWidth += (img_dx * img_vel);
+	initHeight += (img_dy * img_vel);
 
+	if(initWidth + texture.width > screenWidth) {
+		initWidth = screenWidth - area / 2 * 1.2;
+		img_dx = -1;
+	}
+	else if(initWidth < 0) {
+		initWidth = initWidth;
+		img_dx = 1;
+	}
+
+	if(initHeight < 0) {
+		initHeight = initHeight;
+		img_dy = 1;
+	}
+	else if(initHeight + texture.height > screenHeight) {
+		initHeight = screenHeight - area / 2 * 1.2;
+		img_dy = -1;
+	}
+
+}
+
+void renderxp(Image image, Texture2D texture)
+{
+	BeginDrawing();
+	ClearBackground(BLACK);
+	DrawTexture(texture, initWidth, initWidth, WHITE);
+	EndDrawing();
+}
 
 int main(int argc, char *argv[])
 {
-
-	initWidth = screenWidth / 2;
-	initHeight = screenHeight / 2;
 	InitWindow(screenWidth, screenHeight, "DVD");
 
 	SetTargetFPS(60);
